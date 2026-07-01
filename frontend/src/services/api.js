fff3fc080a2fe02d252b597c1ffeff723db216bb
Shreya Mittal <shreyamittal239@gmail.com>
2026-07-01 10:53:24 +0530

    new changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,16 @@
-import axios from "axios";
+/* import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
+  withCredentials: true,
+});
+
+export default api; */
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
