368c3198d0521fd0bc2d569ab9afacb56fcd23bc
Shreya Mittal <shreyamittal239@gmail.com>
2026-07-06 13:27:26 +0530

    finally fixed the error

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -104,7 +104,7 @@ const Dashboard = () => {
 
                 {/* Pie Chart */}
 
-               {/* {dashboardData?.categoryWiseExpense?.length > 0 && (
+                {dashboardData?.categoryWiseExpense?.length > 0 && (
 
                     <div className="mt-8">
 
@@ -114,7 +114,7 @@ const Dashboard = () => {
 
                     </div>
 
-                )} */}
+                )} 
 
                 {/* Recent Expenses */}
 
