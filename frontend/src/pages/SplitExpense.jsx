11b2a2322c63a01f731647b498af0353af80e814
Shreya Mittal <shreyamittal239@gmail.com>
2026-07-06 16:05:51 +0530

    split expense ui

--- frontend/src/pages/SplitExpense.jsx
+++ frontend/src/pages/SplitExpense.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../layouts/DashBoardLayout'
 
 const SplitExpense = () => {
+
+  const [title , setTitle] = useState("");
+  const [totalAmount, setTotalAmount] = useState("");
+  const [ participants , setParticipants] = useState("");
+
+ const addParticipant =() => {
+  setParticipants([...participants, ""]);
+ }
+
+ const handleParticipantChange = (index , value) => {
+   const updatedParticipants = [...participants];
+   updatedParticipant[index] = value;
+   setParticipants(updatedParticipants);
+ }
+
+ const removeParticipant = (index) => {
+   const updatedParticipants = participants.filter (
+    (_ , i) => i !== index
+   );
+
+   setParticipants(updatedParticipants);
+ }
+
   return (
     <DashboardLayout>
         <h1> Dashboard Page </h1>
