587500ca410c4487bcfa5a17c664ed62f313be54
Shreya Mittal <shreyamittal239@gmail.com>
2026-07-09 15:53:13 +0530

    final error

--- backend/src/services/sendEmail.js
+++ backend/src/services/sendEmail.js
@@ -2,8 +2,10 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async(options) => {
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-
+     host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+     family:4,
     auth: {
         user: process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS,
